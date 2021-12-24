7651d16a60a84bc34029dec4c9adaea6bbf31fe4
ayudewita <dewita008@gmail.com>
2021-12-24 09:11:02 +0700

    first-commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '42d19f27feb99599adfbbbaf4b285909'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '68c163e7-ea40-4ab2-854a-6f49bd10192b'
+};
